6ec1a686dee6a20e6eb8f873771b2aa320e9c0d0
C L Bonner <clawrence.bonner@gmail.com>
2022-02-08 11:49:17 +0000

    Create Add-BatchAdUserCSV.ps1

---
+++ Add-BatchAdUserCSV.ps1
@@ -0,0 +1,37 @@
+# Add-BatchADUserCSV - Bulk user account creation script. 
+# This script takes a CSV input file and creates Active Directory accounts with appropriate groups specified in a seperate CSV file.
+# Created by Christopher L Bonner 2022.
+
+if ($args.count -ne 2) {
+    "Usage: Add-BatchADUserCSV {users.csv} {groups.csv}"
+    ""
+    "Users CSV file must contain the following columns: fullname, firstname, lastname, email, password"
+    "Groups CSV file must contain the following column: name"
+    exit
+}
+
+$user_csv = $args[0]
+$group_csv = $args[1]
+
+$users = Import-Csv -Path $user_csv
+$groups = Import-Csv -Path $group_csv
+
+foreach($user in $users) {
+    # get sam account name from email
+    $sam = $user.email.split("@")[0]
+
+    # create account
+    "Creating account for: " + $user.fullname
+    New-ADUser -Name $user.fullname -GivenName $user.firstname -Surname $user.lastname -DisplayName $user.fullname -SamAccountName $sam -EmailAddress $user.email -UserPrincipalName $user.email -Enabled $True -AccountPassword (ConvertTo-SecureString -AsPlainText $user.password -Force) -ChangePasswordAtLogon $True
+    
+    # get new user object
+    $newuser = Get-ADUser $sam
+
+    # add to groups
+    foreach($group in $groups) {
+        "Adding to group: " + $group.name
+        Add-ADGroupMember -Identity $group.name -Members $newuser.ObjectGUID
+    }
+}
+
+"Complete"
